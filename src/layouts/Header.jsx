ebd6ae9efb6effc1be94126c476c4442294ff825
LeHaGiaBao <20520141@gm.uit.edu.vn>
2023-01-02 23:01:44 +0700

    Update project

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TopHeader from './Header/TopHeader'
+
+function Header() {
+	return (
+		<header className='header'>
+			<TopHeader />
+		</header>
+	)
+}
+
+export default Header
