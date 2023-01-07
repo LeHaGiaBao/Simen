2433043486cca89d9105446205c54e7a62998d3b
LeHaGiaBao <20520141@gm.uit.edu.vn>
2023-01-07 23:56:29 +0700

    Add HeaderMenu

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import TopHeader from '../components/Header/TopHeader'
 import BotHeader from '../components/Header/BotHeader'
-// import HeaderMenu from '../components/Header/HeaderMenu'
+import HeaderMenu from '../components/Header/HeaderMenu'
 
 function Header() {
 	return (
 		<header className='header'>
 			<TopHeader />
 			<BotHeader />
-			{/* <HeaderMenu /> */}
+			<HeaderMenu />
 		</header>
 	)
 }
